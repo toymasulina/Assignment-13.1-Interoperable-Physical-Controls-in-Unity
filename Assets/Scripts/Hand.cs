2f8954a1aed9e01589924f4c77e5fc46fb01c469
Robbie Harrison <toymasulina@gmail.com>
2023-05-12 14:13:41 -0400

    Video 13.6 (10:09): Hand Avatars Part 4: Scripting to Hide and Show the Hand

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum HandType
+{
+    Left,
+    Right
+};
+
+public class Hand : MonoBehaviour
+{
+    public HandType handType = HandType.Left;
+    public bool isHidden { get; private set; } = false;
+
+    public InputAction trackedAction = null;
+
+    bool m_isCurrentlyTracked = false;
+
+    List<MeshRenderer> m_currentRenderers = new List<MeshRenderer>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trackedAction.Enable();
+        Hide();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float isTracked = trackedAction.ReadValue<float>();
+        if (isTracked == 1.0f && !m_isCurrentlyTracked)
+        {
+            m_isCurrentlyTracked = true;
+            Show();
+        }
+        else if (isTracked == 0 && m_isCurrentlyTracked)
+        {
+            m_isCurrentlyTracked= false;
+            Hide();
+        }
+    }
+
+    public void Show()
+    {
+        foreach (MeshRenderer renderer in m_currentRenderers)
+        {
+            renderer.enabled = true;
+            m_currentRenderers.Add(renderer);
+        }
+        isHidden = false;
+    }
+
+    public void Hide()
+    {
+        m_currentRenderers.Clear();
+        MeshRenderer[] renderers = GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer renderer in renderers)
+        {
+            renderer.enabled = false;
+            m_currentRenderers.Add(renderer);
+        }
+        isHidden = true;
+    }
+}
